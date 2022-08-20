1c2c8a6d309f4e8a37aacb251796f84d142ae0e8
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2022-08-21 01:27:13 +0200

    create project

---
+++ backend/Go-Bio/src/main/java/com/gobio/gobio/GoBioApplication.java
@@ -0,0 +1,13 @@
+package com.gobio.gobio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoBioApplication.class, args);
+	}
+
+}
---
+++ backend/Go-Bio/src/test/java/com/gobio/gobio/GoBioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobio.gobio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoBioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
